6a0c882ca456d67c7b2ae0df08e3eafed9c35c5b
sawatch-fisherman <sawatch.fisherman@gmail.com>
2025-04-20 10:26:02 +0900

    section-02-09~10

--- vuejs-2025-learning-log/src/main.js
+++ vuejs-2025-learning-log/src/main.js
@@ -1,6 +1,8 @@
-import './assets/main.css'
-
+// 名前付きインポート文
 import { createApp } from 'vue'
+// デフォルトインポート文。viteがvueをjavascriptに変換してインポートしてくれている
 import App from './App.vue'
-
-createApp(App).mount('#app')
+const app = createApp(App)
+// ID="app"の要素を取得して、要素にVueのインスタンスをマウント（挿入）する。
+// このときに既存の要素の内容は削除されて、Vueのマントされた要素が挿入される。
+app.mount('#app')
