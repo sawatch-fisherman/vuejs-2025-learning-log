49724594d48c1693f4ef45f81c53bf80985c6337
sawatch-fisherman <sawatch.fisherman@gmail.com>
2025-05-03 17:22:17 +0900

    section-02-13

--- vuejs-2025-learning-log/src/App.vue
+++ vuejs-2025-learning-log/src/App.vue
@@ -1,8 +1,13 @@
 <script setup>
 const title = 'Vue.js Study'
 let price = 9.99
+function increment() {
+  price += 1
+  console.log(price)
+}
 </script>
 <template>
   <h1>Title: {{ title }}</h1>
   <h2>Price: {{ price - 1 }}</h2>
+  <button @click="increment">button</button>
 </template>
