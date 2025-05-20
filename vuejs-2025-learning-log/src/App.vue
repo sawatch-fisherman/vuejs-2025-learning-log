2a7d95a00a0a3a59d74198d08eff35c4b2a03ab7
sawatch-fisherman <sawatch.fisherman@gmail.com>
2025-05-21 06:32:38 +0900

    section-03-29

--- vuejs-2025-learning-log/src/App.vue
+++ vuejs-2025-learning-log/src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
 import { ref } from 'vue'
 const count = ref(0)
-function countUp(event) {
-  count.value = event.clientX
+function countUp(event, times) {
+  count.value = event.clientX * times
 }
 </script>
 <template>
   <p>{{ count }}</p>
   <button v-on:click="count = $event.clientY">button1</button>
-  <button @click="countUp">button2</button>
+  <button @click="countUp($event, 5)">button2</button>
 </template>
