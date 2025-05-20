26146c3dfdc3d0ec07faa208e68f07be191aa759
sawatch-fisherman <sawatch.fisherman@gmail.com>
2025-05-21 06:04:23 +0900

    section-03-25

--- vuejs-2025-learning-log/src/App.vue
+++ vuejs-2025-learning-log/src/App.vue
@@ -4,5 +4,6 @@ const vueURL = ref('https://vuejs.org/images/logo.png')
 const Id = ref('vue-link')
 </script>
 <template>
-  <a :Id :href="vueURL">Vue.js</a>
+  <a :Id="undefined" :href="false">Vue.js</a>
+  <button :disabled="''">button</button>
 </template>
