bef2b9e75223408050ae785e4d5e52f37422aec7
sawatch-fisherman <sawatch.fisherman@gmail.com>
2025-05-21 06:08:31 +0900

    section-03-26

--- vuejs-2025-learning-log/src/App.vue
+++ vuejs-2025-learning-log/src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
 import { ref } from 'vue'
 const vueURL = ref('https://vuejs.org/images/logo.png')
-const Id = ref('vue-link')
+const vueId = ref('vue-link')
 </script>
 <template>
-  <a :Id="undefined" :href="false">Vue.js</a>
+  <a v-bind="{ id: vueId, href: vueURL }">Vue.js</a>
   <button :disabled="''">button</button>
 </template>
