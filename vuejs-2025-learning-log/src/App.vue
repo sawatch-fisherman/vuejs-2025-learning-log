e052b48deb0e75984c1451e76ec9221543f9d3da
sawatch-fisherman <sawatch.fisherman@gmail.com>
2025-05-24 16:51:38 +0900

    section-03-31

--- vuejs-2025-learning-log/src/App.vue
+++ vuejs-2025-learning-log/src/App.vue
@@ -4,16 +4,5 @@ const count = ref(0)
 </script>
 <template>
   <p>{{ count }}</p>
-  <div @click.stop="count++">
-    <button @click="$event">button1a</button>
-    <button @click="$event.stopPropagation()">button1b not count-up</button>
-    <button @click.stop="">button1c not count-up</button>
-    <a href="https://sawada-system.jp/" @click.prevent="">button1d</a> // イベント修飾子
-  </div>
-
-  <a href="https://sawada-system.jp/" @click="$event.preventDefault()">button2a</a> // ページ遷移しない
-  <a href="https://sawada-system.jp/" @click.prevent="">button2b</a> // イベント修飾子
-
-
-
+  <input type="text" @keyup.space.delete="count++" />
 </template>
