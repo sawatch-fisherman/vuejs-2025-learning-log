9486ace08d23c323ee3a16998332485b7174b8cc
sawatch-fisherman <sawatch.fisherman@gmail.com>
2025-05-25 15:38:27 +0900

    section-03-35

--- vuejs-2025-learning-log/src/App.vue
+++ vuejs-2025-learning-log/src/App.vue
@@ -1,8 +1,15 @@
 <script setup>
-import { ref } from 'vue'
-const count = ref(0)
+import { ref, computed } from 'vue'
+const score = ref(0)
+const evaluation = computed(() => {
+  console.log('computed')
+  return score.value > 3 ? 'Good' : 'Bad'
+})
+console.log(evaluation.value)
 </script>
 <template>
-  <p>{{ count }}</p>
-  <input type="text" @keyup.space.delete="count++" />
+  <p>{{ score > 3 ? 'Good' : 'Bad' }}</p>
+  <p>{{ evaluation }}</p>
+  <p>{{ score }}</p>
+  <button @click="score++">+1</button>
 </template>
