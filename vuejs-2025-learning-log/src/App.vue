824a3d2265ccab38822d30571669c6f84fcf07f3
sawatch-fisherman <sawatch.fisherman@gmail.com>
2025-07-11 06:53:16 +0900

    secsection-04-44

--- vuejs-2025-learning-log/src/App.vue
+++ vuejs-2025-learning-log/src/App.vue
@@ -7,7 +7,5 @@ const myabeOk = ref(false)
 <template>
   <button @click="ok = !ok">Toggle Ok</button>
   <button @click="myabeOk = !myabeOk">Toggle Maybe Ok</button>
-  <p v-if="ok">Ok</p>
-  <p v-else-if="myabeOk">Maybe Ok</p>
-  <p v-else>Not Ok２</p>
+  <p v-show="ok">Ok</p>
 </template>
