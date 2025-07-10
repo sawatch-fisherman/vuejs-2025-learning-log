8376ef679d77140b2e9adf221f6aad226ce0553d
sawatch-fisherman <sawatch.fisherman@gmail.com>
2025-07-11 06:58:46 +0900

    section-04-45

--- vuejs-2025-learning-log/src/App.vue
+++ vuejs-2025-learning-log/src/App.vue
@@ -2,10 +2,9 @@
 import { ref } from 'vue'
 const ok = ref(true)
 const myabeOk = ref(false)
+const animals = ref(['cat', 'dog', 'fish'])
 
 </script>
 <template>
-  <button @click="ok = !ok">Toggle Ok</button>
-  <button @click="myabeOk = !myabeOk">Toggle Maybe Ok</button>
-  <p v-show="ok">Ok</p>
+  <li v-for="animal in animals">{{ animal }}</li>
 </template>
