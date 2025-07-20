0c7100faa858331ee93a10a1df2530bee87b0f93
sawatch-fisherman <sawatch.fisherman@gmail.com>
2025-07-21 07:29:56 +0900

    section-04-46

--- vuejs-2025-learning-log/src/App.vue
+++ vuejs-2025-learning-log/src/App.vue
@@ -1,10 +1,17 @@
 <script setup>
 import { ref } from 'vue'
-const ok = ref(true)
-const myabeOk = ref(false)
-const animals = ref(['cat', 'dog', 'fish'])
+// ↓変な動作するパターン（全部のテキストボックスに値を入力して、buttonを押すと、1つ目のテキストボックスの値が残る）
+// const animals = ref([{ id: 2, name: 'Dog' }, { id: 2, name: 'Dog' }, { id: 3, name: 'Bird' }])
 
+// 正しく動作するパターン
+const animals = ref([{ id: 2, name: 'Dog' }, { id: 2, name: 'Dog' }, { id: 3, name: 'Bird' }])
 </script>
 <template>
-  <li v-for="animal in animals">{{ animal }}</li>
+  <button @click="animals.shift()">button</button>
+  <!-- v-forを使用する場合は、 :key が必須になる -->
+  <!-- :keyが無いと、Elements in iteration expect to have 'v-bind:key' directives.といったエラーが表示される -->
+  <!-- https://vuejs.org/guide/essentials/list.html#maintaining-state-with-key -->
+  <li v-for="animal in animals" :key="animal.id"><input type="text" />{{ animal.name }}</li>
+  <!-- ↓NGなパターン -->
+  <!-- <li v-for="animal in animals"><input type="text" />{{ animal.name }}</li> -->
 </template>
