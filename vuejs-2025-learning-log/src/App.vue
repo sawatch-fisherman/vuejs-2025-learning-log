0ecaa649e2468a6561d6e0d0b8ffa8fc4913f0ae
sawatch-fisherman <sawatch.fisherman@gmail.com>
2025-07-21 07:47:42 +0900

    section-04-48

--- vuejs-2025-learning-log/src/App.vue
+++ vuejs-2025-learning-log/src/App.vue
@@ -1,18 +1,13 @@
 <script setup>
 import { ref } from 'vue'
-// ↓変な動作するパターン（全部のテキストボックスに値を入力して、buttonを押すと、1つ目のテキストボックスの値が残る）
-// const animals = ref([{ id: 2, name: 'Dog' }, { id: 2, name: 'Dog' }, { id: 3, name: 'Bird' }])
-
-// 正しく動作するパターン
 const animals = ref([{ id: 1, name: 'Dog' }, { id: 2, name: 'Cat' }, { id: 3, name: 'Bird' }])
 </script>
 <template>
   <button @click="animals.shift()">button</button>
-  <!-- v-forを使用する場合は、 :key が必須になる -->
-  <!-- :keyが無いと、Elements in iteration expect to have 'v-bind:key' directives.といったエラーが表示される -->
-  <!-- https://vuejs.org/guide/essentials/list.html#maintaining-state-with-key -->
+  <!-- 書き方その１ -->
   <li v-for="(animal, index) in animals" :key="animal.id"><input type="text" />{{ animal.name }}({{ index }})
   </li>
-  <!-- ↓NGなパターン -->
-  <!-- <li v-for="animal in animals"><input type="text" />{{ animal.name }}</li> -->
+  <!-- 書き方その２(Javascriptの分割代入みたいな書き方 -->
+  <li v-for="({ id, name }, index) in animals" :key="id"><input type="text" />{{ name }}({{ index }})
+  </li>
 </template>
