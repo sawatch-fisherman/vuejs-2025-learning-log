65730421963a84a18521ba24bce61af07ba7ea25
sawatch-fisherman <sawatch.fisherman@gmail.com>
2025-07-24 06:04:01 +0900

    section-04-51

--- vuejs-2025-learning-log/src/App.vue
+++ vuejs-2025-learning-log/src/App.vue
@@ -1,11 +1,7 @@
 <script setup>
 import { ref } from 'vue'
-const animals = ref({
-  name: 'dog',
-  age: 3,
-  weight: 10,
-})
 </script>
 <template>
-  <p v-for="(value, key, index) in animals" :key="value">{{ key }}: {{ value }} ({{ index }})</p>
+  <!-- 0からではなく1から始まる -->
+  <p v-for="n in 10" :key="n">{{ n }}</p>
 </template>
