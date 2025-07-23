26eae7dbb2193ffb06fc20d8cc56e0050e7f56af
sawatch-fisherman <sawatch.fisherman@gmail.com>
2025-07-24 06:02:12 +0900

    section-04-50

--- vuejs-2025-learning-log/src/App.vue
+++ vuejs-2025-learning-log/src/App.vue
@@ -1,14 +1,11 @@
 <script setup>
 import { ref } from 'vue'
-const animals = ref([{ id: 1, name: 'Dog' }, { id: 2, name: 'Cat' }, { id: 3, name: 'Bird' }])
+const animals = ref({
+  name: 'dog',
+  age: 3,
+  weight: 10,
+})
 </script>
 <template>
-  <button @click="animals.shift()">button</button>
-  <!-- v-forとv-ifは混合して書かない！v-forを先に1行使用して定義する -->
-  <template v-for="({ id, name }, index) in animals" :key="id">
-    <template v-if="id === 1">
-      <p>id: {{ id }}</p>
-      <input type="text" />{{ name }}({{ index }})
-    </template>
-  </template>
+  <p v-for="(value, key, index) in animals" :key="value">{{ key }}: {{ value }} ({{ index }})</p>
 </template>
