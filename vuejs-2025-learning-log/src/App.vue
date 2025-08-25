f2409cab8501b7b0d2c9eaff39de702bb38cda63
sawatch-fisherman <sawatch.fisherman@gmail.com>
2025-08-26 06:53:10 +0900

    section-06-65

--- vuejs-2025-learning-log/src/App.vue
+++ vuejs-2025-learning-log/src/App.vue
@@ -1,12 +1,9 @@
 <script setup>
 import { ref } from 'vue';
 import ShowCount from './components/ShowCount.vue';
-const count = ref(0);
 </script>
 <template>
   <div>
-    <!-- 以下の書き方も同じ<ShowCount :foo="count" hello="こんにちわ"/> -->
-    <ShowCount v-bind="{ foo: count, hello: 'こんにちわ' }" />
-    <button @click="count++">加算</button>
+    <ShowCount />
   </div>
 </template>
--- vuejs-2025-learning-log/src/components/ShowCount.vue
+++ vuejs-2025-learning-log/src/components/ShowCount.vue
@@ -1,22 +1,12 @@
 <script setup>
-import { watch, watchEffect } from 'vue';
-
-const { foo, hello } = defineProps(['foo', 'hello']);
-
-watchEffect(() => {
-  console.log(foo);
-})
-// watchは必ず第一引数は関数で記述する必要がある
-watch(
-  // foo, この書き方はNG
-  () => foo,  // この書き方はOK
-  () => {
-    console.log('watch');
+defineProps({
+  foo: {
+    type: Number,
+    default: 100
   }
-)
+})
 
 </script>
 <template>
-  <p>Count: {{ foo }}</p>
-  <p>挨拶:{{ hello }}</p>
+  <p>カウント: {{ foo }}</p>
 </template>
