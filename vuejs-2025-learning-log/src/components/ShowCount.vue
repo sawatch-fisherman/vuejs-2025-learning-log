4f0b764b369f1b190838374209a24cb5a3dc91bd
sawatch-fisherman <sawatch.fisherman@gmail.com>
2025-08-26 07:07:18 +0900

    section-06-66

--- vuejs-2025-learning-log/src/components/ShowCount.vue
+++ vuejs-2025-learning-log/src/components/ShowCount.vue
@@ -1,8 +1,23 @@
 <script setup>
+const isRequired = false
+// defineProps内では、外部（スコープ外）の値(isRequired)を参照できない
 defineProps({
   foo: {
-    type: Number,
-    default: 100
+    // 以下のように[]で囲むと複数の型を指定できる
+    type: [Number, String, Boolean, Array, Object, Function, Symbol],
+
+    // 以下のように外部の変数を参照できない
+    //required: isRequired,
+
+    // javaScriptのルールで、オブジェクトや配列は、必ず関数で返り値を返すようにする
+    default: function () {
+      return {}
+    },
+
+    // 以下のように値のバリデーションも可能
+    validate: function (value) {
+      return value === 0 || value === 1
+    }
   }
 })
 
