f5a05dea67d15d67b9000c967e5a518dfa836599
sawatch-fisherman <sawatch.fisherman@gmail.com>
2025-09-15 07:58:21 +0900

    section-06-70

--- vuejs-2025-learning-log/src/components/FormatButton.vue
+++ vuejs-2025-learning-log/src/components/FormatButton.vue
@@ -1,6 +1,16 @@
+<script setup>
+// 以下は無くても動作する。よって、そんなに重要ではないが、どんなイベントを発火するかを明示的にするために書くことがある
+// defineEmits(['format']);
+
+// スクリプトからエミットする場合は、defineEmitsの返り値を受け取る
+// 返り値の型を指定する場合は、以下のようにオブジェクト形式で書く
+const emit = defineEmits(['format']);
+function emitFormat() {
+  emit('format', 100);
+}
+</script>
 <template>
   <!-- 子コンポーネントからは、あくまでイベントを発火するだけ
   実処理は親コンポーネントで行う -->
-  <!-- 値を渡すこともできる -->
-  <button @click="$emit('format', 100)">初期化</button>
+  <button @click="emitFormat">初期化</button>
 </template>
