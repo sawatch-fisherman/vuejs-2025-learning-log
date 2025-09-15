78479cccf0bcb34a5cba6766dc9e374506ad8187
sawatch-fisherman <sawatch.fisherman@gmail.com>
2025-09-15 17:07:20 +0900

    section-07-74

--- vuejs-2025-learning-log/src/App.vue
+++ vuejs-2025-learning-log/src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
+import MenyCard from './components/MenyCard.vue'
 </script>
 <template>
   <main>
-    <h1>今日は何を食べる？</h1>
+    <h1 class="title">今日は何を食べる？</h1>
+    <MenyCard />
+    <p>VS</p>
+    <MenyCard />
   </main>
 </template>
 
@@ -11,5 +15,9 @@ main {
   width: 350px;
   margin: 100px auto;
   text-align: center;
+}
+
+.title {
+  margin-bottom: 50px;
 }
 </style>
---
+++ vuejs-2025-learning-log/src/components/MenyCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card">
+    <div>カレー</div>
+    <div>🍛</div>
+  </div>
+</template>
+<style scoped>
+.card {
+  box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.3);
+  border-radius: 5px;
+  font-size: 2rem;
+  padding: 20px;
+  cursor: pointer;
+}
+</style>
