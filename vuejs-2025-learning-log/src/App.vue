df0ab0413a2a8814cdc558e316354351f39ad467
sawatch-fisherman <sawatch.fisherman@gmail.com>
2025-09-17 07:40:38 +0900

    section-08-81

--- vuejs-2025-learning-log/src/App.vue
+++ vuejs-2025-learning-log/src/App.vue
@@ -1,47 +1,11 @@
 <script setup>
 import { ref } from 'vue'
-import MenyCard from './components/MenyCard.vue'
-const menyus = ref([
-  { name: 'カレー', emoji: '🍛' },
-  { name: 'カニ', emoji: '🦀' },
-  { name: 'ラーメン', emoji: '🍜' },
-  { name: '寿司', emoji: '🍣' },
-  { name: 'ピザ', emoji: '🍕' },
-  { name: 'ハンバーガー', emoji: '🍔' },
-  { name: 'サラダ', emoji: '🥗' },
-  { name: 'ステーキ', emoji: '🥩' },
-  { name: 'アイスクリーム', emoji: '🍨' },
-  { name: 'ドーナツ', emoji: '🍩' },
-])
-const selectedMenyusIndex = ref(0)
-const recommendMenyusIndex = ref(1)
-function selectMeny(index) {
-  selectedMenyusIndex.value = index
-  recommendMenyusIndex.value++
-}
+import CountNumber from './components/CountNumber.vue'
+const isDisplay = ref(true)
 </script>
 <template>
-  <main>
-    <h1 class="title">今日は何を食べる？</h1>
-    <MenyCard :name="menyus[selectedMenyusIndex].name" :emoji="menyus[selectedMenyusIndex].emoji"
-      @click="selectMeny(selectedMenyusIndex)" />
-    <template v-if="recommendMenyusIndex < menyus.length">
-      <p>VS</p>
-      <MenyCard :name="menyus[recommendMenyusIndex].name" :emoji="menyus[recommendMenyusIndex].emoji"
-        @click="selectMeny(recommendMenyusIndex)" />
-    </template>
-
-  </main>
+  <div>
+    <button @click="isDisplay = !isDisplay">表示/非表示</button>
+    <CountNumber v-if="isDisplay" />
+  </div>
 </template>
-
-<style scoped>
-main {
-  width: 350px;
-  margin: 100px auto;
-  text-align: center;
-}
-
-.title {
-  margin-bottom: 50px;
-}
-</style>
