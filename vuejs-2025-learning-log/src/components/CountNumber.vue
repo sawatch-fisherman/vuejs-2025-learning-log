dcaa13f254b3d4d726a7d0464a357067f84943ce
sawatch-fisherman <sawatch.fisherman@gmail.com>
2025-09-19 07:39:22 +0900

    section-08-81-2

--- vuejs-2025-learning-log/src/components/CountNumber.vue
+++ vuejs-2025-learning-log/src/components/CountNumber.vue
@@ -1,6 +1,29 @@
 <script setup>
-import { ref } from 'vue'
+import { onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted, ref } from 'vue'
 const count = ref(0)
+onBeforeMount(() => {
+  console.log('CountNumberコンポーネントが(前)マウントされました')
+  // ↓デバッグモードになる（ブレークポイントをブラウザのデバッグツールで設定できる）
+  // ↓の段階では、コンポーネントがマウントされていないので、countは非表示になっている
+  // debugger
+})
+onMounted(() => {
+  console.log('CountNumberコンポーネントが(後)マウントされました')
+  // ↓の段階では、コンポーネントがマウントされているので、countは表示されている
+  // debugger
+})
+onBeforeUpdate(() => {
+  console.log('CountNumberコンポーネントが(前)更新されました')
+})
+onUpdated(() => {
+  console.log('CountNumberコンポーネントが(後)更新されました')
+})
+onBeforeUnmount(() => {
+  console.log('CountNumberコンポーネントが(前)アンマウントされました')
+})
+onUnmounted(() => {
+  console.log('CountNumberコンポーネントが(後)アンマウントされました')
+})
 </script>
 <template>
   <div>
