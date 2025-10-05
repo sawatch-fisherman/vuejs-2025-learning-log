7c2c5242d5aa4b7b366234c69d509761ee129b23
sawatch-fisherman <sawatch.fisherman@gmail.com>
2025-10-06 07:05:53 +0900

    section-09-85

--- vuejs-2025-learning-log/src/App.vue
+++ vuejs-2025-learning-log/src/App.vue
@@ -1,15 +1,22 @@
 <script setup>
-import { onMounted, useTemplateRef } from 'vue'
+import { ref } from 'vue'
+import DefaultCard from './components/DefaultCard.vue'
 
-// 本物のDOM要素を参照するためのrefを作成する
-const userInputRef = useTemplateRef('userInput')
-console.log(userInputRef.value)
-onMounted(() => {
-  // リロードすると、input要素にフォーカスが当たる
-  userInputRef.value.focus()
-})
+const y = ref('y')
 </script>
 <template>
-  <!-- 本物のDOM要素を参照するためのrefを作成する -->
-  <input type="text" ref="userInput" />
+  <h1>Slot</h1>
+  <DefaultCard>
+    <h2>Hello World2</h2>
+    <p>I'm fain!</p>
+    <!-- refで未定義の場合は、エラーになる。 -->
+    <p>{{ x }}</p>
+    <!-- refで定義した場合は、エラーにならない。 -->
+    <p>{{ y }}</p>
+  </DefaultCard>
+  <!-- Slotを使えば値を動的に変えられる。 -->
+  <DefaultCard>
+    <h2>Hello World3</h2>
+    <p>I'm fain!2</p>
+  </DefaultCard>
 </template>
---
+++ vuejs-2025-learning-log/src/components/DefaultCard.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+const x = ref('x')
+</script>
+
+<template>
+  <div class="card">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+div {
+  box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.3);
+  border-radius: 5px;
+  padding: 20px;
+}
+</style>
