ad12c1c2a81b96c988b4b11fdda2ee4f24f965a5
sawatch-fisherman <sawatch.fisherman@gmail.com>
2025-10-19 08:03:35 +0900

    section-09-89

--- vuejs-2025-learning-log/src/App.vue
+++ vuejs-2025-learning-log/src/App.vue
@@ -1,31 +1,18 @@
 <script setup>
-import DefaultCard from './components/DefaultCard.vue'
+import { ref } from 'vue'
+import CompA from './components/CountNumber.vue'
+import CompB from './components/DefaultCard.vue'
+import CompC from './components/MenyCard.vue'
+const currentComp = ref(CompA)
 
 </script>
 <template>
-  <h1>Slot</h1>
-  <DefaultCard>
-    <!-- </slot name="default" />を子コンポーネントで定義していると、↓<p>タグのコンテンツが表示される。 -->
-    <!-- ちなみに<p>タグは、どこに複数書いても、ひとまとめにされて</slot name="default" />の定義位置になる -->
-    <p>Default Content1</p>
-    <!-- <template v-slot:header></template> -->
-    <!-- v-slot:headerと#headerは同じ意味。eslintは#の方を推奨。 -->
-    <!-- スロットプロップスは、子コンポーネントで定義していると、親コンポーネントで使用できる。 -->
-    <template #header="{ pageCount }">
-      <h2 v-if="pageCount === 1">いぬ</h2>
-      <h2 v-if="pageCount === 2">ねこ</h2>
-      <h2 v-if="pageCount === 3">とら</h2>
-    </template>
-    <template #main>
-      <p>Main Content</p>
-    </template>
-    <template #footer>
-      <p>Footer Content</p>
-    </template>
-    <!-- <p>タグは、どこに複数書いても、ひとまとめにされて</slot name="default" />の定義位置になる -->
-    <p>Default Content2</p>
-
-  </DefaultCard>
-  <!-- slotで定義しないとデフォルトのコンテンツが表示される。 -->
+  <h1>動的コンポーネント</h1>
+  <button @click="currentComp = CompA">A</button>
+  <button @click="currentComp = CompB">B</button>
+  <button @click="currentComp = CompC">C</button>
+  <!-- 動的にコンポーネントを入れ替えることができる -->
+  <!-- この状態だとブラウザのコンソールにエラーが表示されている -->
+  <component :is="currentComp" />
 
 </template>
